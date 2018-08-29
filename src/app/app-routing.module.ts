712c42211867c9e281bf1ebde047a4f9364f88b6
fredli53016 <fredli53016@gmail.com>
2018-08-29 20:41:26 +1000

    move  to user folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent} from "./homepage/homepage.component";
 import { MyCenterComponent } from "./my-center/my-center.component";
 import { ActivityComponent } from "./activity/activity.component";
-import { LoginComponent } from "./login/login.component";
-import { SignUpComponent} from "./sign-up/sign-up.component";
+import { LoginComponent } from "./user/login/login.component";
+import { SignUpComponent} from "./user/sign-up/sign-up.component";
 
 const routes: Routes = [
     { path: 'homepage', component: HomepageComponent },
